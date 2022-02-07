a088c1136016c1104ac451a0efacb3655ac79cc7
harunakaratsu <haruna.karatsu@gmail.com>
2022-02-07 17:00:01 +0900

    Add: Foodのテストを追加

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -1,5 +1,59 @@
 require 'rails_helper'
 
 RSpec.describe Food, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "全てのフィールドが有効な場合" do
+    it '有効であること' do 
+      food = build(:food)
+      expect(food).to  be_valid
+    end
+  end
+
+  context "名前が存在しない場合" do
+    it '無効であること' do
+      food = build(:food, name: '')
+      expect(food).to  be_invalid
+    end
+  end
+
+  context "名前が50文字以上である場合" do
+    it '無効であること' do
+      food = build(:food, name: 'a' * 51)
+      expect(food).to  be_invalid
+    end
+  end
+
+  context "金額が存在しない場合" do
+    it '無効であること' do
+      food = build(:food, price: nil)
+      expect(food).to  be_invalid
+    end
+  end
+
+  context "金額が0以下である場合" do
+    it '無効であること' do
+      food = build(:food, price: 0)
+      expect(food).to  be_invalid
+    end
+  end
+
+  context "カロリーが存在しない場合" do
+    it '無効であること' do
+      food = build(:food, calorie: nil)
+      expect(food).to  be_invalid
+    end
+  end
+
+  context "カロリーが0以下である場合" do
+    it '無効であること' do
+      food = build(:food, calorie: 0)
+      expect(food).to  be_invalid
+    end
+  end
+
+  context "ユーザーが存在しない場合" do
+    it '無効であること' do
+      food = build(:food, user_id: nil)
+      expect(food).to  be_invalid
+    end
+  end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,14 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-  describe "GET /index" do
-    describe "POST /users" do
-      let!(:user) { create(:user) }
-  
-      it "statusが200であること" do
-        post users_path, params: { idToken: 'idToken' }
-        expect(response.status).to eq(200)
-      end
+  describe "POST /users" do
+    it "statusが200であること" do
+      post users_path, params: { idToken: 'idToken' }
+      expect(response.status).to eq(200)
     end
   end
 end
