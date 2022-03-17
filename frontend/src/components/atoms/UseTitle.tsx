0fe8ceedef4ea9b1be049c97f675ff8f4c69fade
harunakaratsu <haruna.karatsu@gmail.com>
2022-03-17 20:34:43 +0900

    Fix: トップページと使い方画面を修正

---
+++ frontend/src/components/atoms/UseTitle.tsx
@@ -0,0 +1,16 @@
+import { memo, VFC } from "react"
+import { Heading } from "@chakra-ui/react"
+
+type Props = {
+  children: string
+}
+
+export const UseTitle: VFC<Props> = memo((props) => {
+  const { children } = props
+
+  return (
+    <Heading as='h4' size='md' mb={5} style={{ background: "#a1a1a1", color: "#fff", padding: "20px", borderRadius:"4px", marginBottom: "60px" }}>
+      { children }
+    </Heading>
+  )
+})
